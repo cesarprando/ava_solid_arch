0f58f1a0e08fa9a4b86b67c2ddb4b28b7e313d49
cesarprando <cesarprandosz@gmail.com>
2026-05-11 14:12:41 -0300

    Task 2: Criação do Pet com Upload Múltiplo (create)

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -9,6 +9,73 @@ module.exports = class PetController {
 
     static async create(req, res) {
 
+        const { name, age, weight, color } = req.body
+
+        const images = []
+
+        if (req.files) {
+            req.files.forEach((image) => {
+                images.push(image.filename)
+            })
+        }
+
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório!' })
+            return
+        }
+
+        if (!age) {
+            res.status(422).json({ message: 'A idade é obrigatória!' })
+            return
+        }
+
+        if (!weight) {
+            res.status(422).json({ message: 'O peso é obrigatório!' })
+            return
+        }
+
+        if (!color) {
+            res.status(422).json({ message: 'A cor é obrigatória!' })
+            return
+        }
+
+        if (images.length === 0) {
+            res.status(422).json({ message: 'A imagem é obrigatória!' })
+            return
+        }
+
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pet = new Pet({
+            name,
+            age,
+            weight,
+            color,
+            images,
+            available: true,
+            user: {
+                _id: user._id,
+                name: user.name,
+                phone: user.phone,
+                image: user.image,
+            }
+        })
+
+        try {
+
+            const newPet = await pet.save()
+
+            res.status(201).json({
+                message: 'Pet cadastrado com sucesso!',
+                pet: newPet
+            })
+
+        } catch (error) {
+            res.status(500).json({
+                message: error
+            })
+        }
     }
 
     static async getAll(req, res) {
